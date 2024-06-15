c6d3936ba9b5a300160ac2bcb7e50788005d6b25
vijenderyuadav944 <vijenderyadav944@gmail.com>
2024-06-15 17:25:15 +0530

    first commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,84 @@
+/**
+ * About component
+ *
+ * Space for you to describe more about yourself.
+ */
+
+import React from "react";
+
+/**
+ * About background image
+ *
+ * Below is a sample image. Upload the image of your choice into the "images"
+ * directory and import here for use. Then, set imageAltText to string that 
+ * represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a image you
+ * freely use on your site.
+ */
+import image from "../images/motion-background.jpg";
+
+const imageAltText = "purple and blue abstract background";
+
+/**
+ * Sort description that expands on your title on the Home component.
+ */
+const description =
+  "I am a full stack web developer with a passion for creating beautiful and functional user interfaces. I have experience in both front-end and back-end development, and I am always looking for new ways to improve my skills. I am currently learning more about UI/UX design and SEO optimization to create better user experiences. I am excited to continue learning and growing in the tech industry.";
+
+/**
+ * List of some of skills or technologies you work on, are learning,
+ * passionate about, or enjoy,
+ */
+const skillsList = [
+  "Front-end development",
+  "back-end development",
+  "UI/UX design",
+  "SEO optimization",
+];
+
+/**
+ * Use this to give more information about what you are passionate about,
+ * how you best work, or even a quote. This will help someone learn more
+ * about you on a professional level.
+ */
+const detailOrQuote =
+  "I am passionate about creating beautiful and functional user interfaces that provide a great user experience. I am always looking for new ways to improve my skills and learn new technologies. I believe that the best way to learn is by doing, so I am constantly working on new projects to expand my knowledge and improve my skills. I am excited to continue growing in the tech industry and see where my skills can take me.";
+
+const About = () => {
+  return (
+    <section className="padding" id="about">
+      <img className="background" src={image} alt={imageAltText} />
+      <div
+        style={{
+          backgroundColor: "white",
+          width: "50%",
+          padding: "4rem",
+          margin: "3rem auto",
+          textAlign: "center",
+        }}
+      >
+        <h2>About Myself</h2>
+        <p className="large">{description}</p>
+        <hr />
+        <ul
+          style={{
+            textAlign: "left",
+            columns: 2,
+            fontSize: "1.25rem",
+            margin: "2rem 3rem",
+            gap: "3rem",
+          }}
+        >
+          {skillsList.map((skill) => (
+            <li key={skill}>{skill}</li>
+          ))}
+        </ul>
+        <hr />
+        <p style={{ padding: "1rem 3rem 0" }}>{detailOrQuote}</p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/Components/Portfolio.jsx
@@ -0,0 +1,86 @@
+/**
+ * Portfolio component
+ *
+ * Highlights some of  your creations. These can be designs, websites,
+ * open source contributions, articles you've written and more.
+ *
+ * This is a great area for you to to continually add to and refine
+ * as you continue to learn and create.
+ */
+
+import React from "react";
+
+/**
+ * Desk image
+ *
+ * Below is a sample desk image. Feel free to update this to an image of your choice,
+ * updating below imageAltText to string that represents what you see in that image.
+ *
+ * Need an image? Check out https://unsplash.com to download a photo you
+ * freely use on your site.
+ */
+import image from "../images/design-desk.jpeg";
+
+const imageAltText = "desktop with books and laptop";
+
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+const projectList = [
+  {
+    title: "Streamify",
+    description:
+      "An online meeting platform that allows users to create and join video calls with friends and family. Users can also create playlists and listen to music together.",
+    url: "https://streamify-gules.vercel.app/",
+  },
+  {
+    title: "Web Development for Beginners",
+    description:
+      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+    url: "https://github.com/microsoft/web-dev-for-beginners",
+  },
+  {
+    title: "My Portfolio Site",
+    description:
+      "Built my portfolio site using React and deployed it using Vercel.",
+    url: "https://portfolio-ten-beige-80.vercel.app/",
+  },
+  {
+    title: "ProConnect",
+    description:
+      "A social media platform that allows users to connect with other professionals in their field. Users can create a profile, share posts, and connect with other users.",
+    url: "https://proconnect-theta.vercel.app/",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section className="padding" id="portfolio">
+      <h2 style={{ textAlign: "center" }}>Portfolio</h2>
+      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <img
+            src={image}
+            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            alt={imageAltText}
+          />
+        </div>
+        <div className="container">
+          {projectList.map((project) => (
+            <div className="box" key={project.title}>
+              <a href={project.url} target="_blank" rel="noopener noreferrer">
+                <h3 style={{ flexBasis: "40px" }}>{project.title}</h3>
+              </a>
+              <p className="small">{project.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
